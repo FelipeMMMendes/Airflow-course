0887c1bb8b0b133b2a6daa2bcb197d030e053d84
Felipe Martins <96206410+FelipeMMMendes@users.noreply.github.com>
2023-10-31 11:40:27 -0300

    created the dummy dag

---
+++ dummy.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.task_group import TaskGroup
+from datetime import datetime
+from airflow.operators.dummy_operator import DummyOperator
+
+#instanciando a dag
+dag = DAG('dummy', description="dummy",
+          schedule_interval=None,
+          start_date=datetime(2023,3,5),
+          catchup=False
+          )
+
+    
+task1 = BashOperator(task_id="tsk1", bash_command="sleep 1", dag=dag)
+task2 = BashOperator(task_id="tsk2", bash_command="sleep 1", dag=dag)
+task3 = BashOperator(task_id="tsk3", bash_command="sleep 1", dag=dag)
+task4 = BashOperator(task_id="tsk4", bash_command="sleep 1", dag=dag)
+task5 = BashOperator(task_id="tsk5", bash_command="sleep 1", dag=dag)    
+taskdummy = DummyOperator(task_id='tsk_dummy', dag=dag)
+
+
+[task1, task2, task3] >> taskdummy >> [task4,task5]
